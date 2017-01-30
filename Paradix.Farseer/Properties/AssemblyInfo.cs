e7707ff9124fb7aa815558c8111466e5c8409a05
Swann Jaunasse <swann.jaunasse@epitech.eu>
2017-01-30 18:12:26 +0100

    Moved Farseer Into Separated Project

---
+++ Paradix.Farseer/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle ("Paradix.Farseer")]
+[assembly: AssemblyDescription ("Farseer 2D C# Physics Engine")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyVersion ("1.0.*")]
