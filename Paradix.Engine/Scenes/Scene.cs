03514aa60e7e98933991345636de6d9e3b6c4b37
Swann Jaunasse <swann.jaunasse@epitech.eu>
2017-02-07 22:49:07 +0100

    Started SceneManager Implementation

---
+++ Paradix.Engine/Scenes/Scene.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace Paradix.Engine
+{
+	public abstract class Scene : IUpdateable, IDrawable, IContent
+	{
+		public string Name { get; private set; } = null;
+		public List<Entity> Entities { get; private set; } = null;
+		public bool IsActive { get; set; } = true;
+		public bool IsPlaying { get; internal set; } = false;
+
+		protected Scene(string name)
+		{
+			Contract.RequiresNotEmpty (name, "name");
+
+			Name = name;
+			Entities = new List<Entity> ();
+		}
+
+		public void Load (ContentManager content)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Update (GameTime gameTime, InputManager input)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Draw (GameTime gameTime, GraphicsManager graphics)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Destroy ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
---
+++ Paradix.Engine/Scenes/SceneManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace Paradix.Engine
+{
+	public class SceneManager : IUpdateable, IDrawable, IContent
+	{
+		public Scene CurrentScene { get; private set; }
+
+		public SceneManager ()
+		{
+		}
+
+		public void Play (string sceneName)
+		{
+			
+		}
+
+		public void AddScene (Scene scene)
+		{
+			
+		}
+
+		public void Load (ContentManager content)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Update (GameTime gameTime, InputManager input)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Draw (GameTime gameTime, GraphicsManager graphics)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Destroy ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
